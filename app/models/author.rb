7a4ce442a88178f2ed8f7afd73a68377d227b092
nishanth.mosam <nishanth.mosam@gmail.com>
2014-05-09 13:57:20 +0530

    last

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,13 +7,24 @@ class Author < ActiveRecord::Base
 
 
     def create_uniquename
+    auth=Author.find(:all,:conditions => ['unique_name IN (?)', self.name] )
+    if auth.count==1
+    b=0
+    b=b+1
+    a=self.name
+    a=a+"b"
+    self.update_attributes(
+      :unique_name  => "#{a}"
+    )
+    else
     self.update_attributes(
       :unique_name  => self.name
     )
     end
+    end
 
      def to_param
-    "#{id} #{unique_name}".parameterize
+    "#{unique_name}".parameterize
   end
 
 
