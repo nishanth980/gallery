f54fc8711302a43bdb6932913f9ba78302106636
nishanth980 <nishanth.mosam@gmail.com>
2014-05-17 09:32:13 +0530

    lst

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(:version => 20140516133216) do
   create_table "images", :force => true do |t|
     t.text     "title"
     t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
